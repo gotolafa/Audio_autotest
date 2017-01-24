867107eba0c53674c15a2be7b47733076ea2d90b
tony.tsou <tony.tsou@amtran.com.tw>
2017-01-24 14:41:42 +0800

    Add test.py for test

---
+++ test.py
@@ -0,0 +1,30 @@
+import re
+import os
+import sys
+import shutil
+import chardet
+import time
+
+#----------------------------------------------------------------------
+def Audio_Loop():
+    count = 0
+    print "Start test"
+
+    os.system('omxplayer -p -o hdmi Pink_pcm.wav')
+    time.sleep(7)
+    os.system('omxplayer -p -o hdmi Pink_ac3.ac3')
+    time.sleep(7)
+    os.system('omxplayer -p -o hdmi Pink_dts.dtshd')
+    time.sleep(7)    
+    
+
+#----------------------------------------------------------------------
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        print 'Audio Test'
+        sys.exit()
+
+    if(sys.argv[1] == 'test'):
+        Audio_Loop()
+    
